853c2ae933385e81b242b7898960538cfb083be1
SebastianRos <sebastian-rosauer@outlook.de>
2018-05-31 18:30:33 +0200

    speichern vergessenn

--- U2_Nr2_Bruch/konsolenoberflaeche.c
+++ U2_Nr2_Bruch/konsolenoberflaeche.c
@@ -7,17 +7,31 @@ struct vars{
     Rational b;
 };
 struct commands{
+    char print[10];
     char set[10];
+    char reset[10];
+    char execute[10];
+    char exit[10];
 };
-static struct commands cmdlist = {"set"};
+static struct commands cmdlist = {"print", "set", "reset", "execute", "exit"};
 
 int main(){
     while (1){
         char cmd[10];
         scanf("%s", &cmd);
-        if (strcmp(cmd, cmdlist.set) == 0){
+        if (strcmp(cmd, cmdlist.print) == 0){
+            printf("print\n");
+        } else if (strcmp(cmd, cmdlist.set) == 0){
             printf("set\n");
+        } else if (strcmp(cmd, cmdlist.reset) == 0){
+            printf("reset\n");
+        } else if (strcmp(cmd, cmdlist.execute) == 0){
+            printf("execute\n");
+        } else if (strcmp(cmd, cmdlist.exit) == 0){
+            printf("exit\n");
+            return 0;
+        } else{
+            printf("Der Befehl %s ist unbekannt.\n", cmd);
         }
-        return 0;
     }
 }
